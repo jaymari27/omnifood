b274660d881b0f2546e1b6c5053882d682710e74
jaymari27 <87401114+jaymari27@users.noreply.github.com>
2021-07-14 09:07:33 +0800

    Add files via upload
    Added files

---
+++ resources/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    /* js files must be started with this line ↑ */
+    /* this will make the code run after the document has been loaded */
+    /*var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+}) */
+});
